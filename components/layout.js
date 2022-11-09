02bf8aca581b15ead1951af2a434f65b7a696817
Joshua Maza <joshuasadirimaza@gmail.com>
2022-11-09 15:11:05 -0800

    fixing image path typo

--- components/layout.js
+++ components/layout.js
@@ -16,7 +16,7 @@ export default function Layout({ children, home }) {
             <a>
               <Image
                 priority
-                src="/images/profile.png"
+                src="/images/profile.PNG"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
